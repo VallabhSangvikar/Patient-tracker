3e737183b7edd16ab34dc35416370a0f882d5352
Rajparikh18 <parikhraj85@gmail.com>
2024-04-25 23:52:16 +0530

    schema create

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port=4000;
 const methodOverride=require("method-override");
 app.use(methodOverride("_method"));
 
+const records= require("./models/records");
 
 app.use(express.urlencoded({extended:true}));
 
@@ -16,7 +17,9 @@ app.use(express.static(path.join(__dirname,"public")));
 
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
+main()
+.then(()=>{console.log("mongo running")})
+.catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/patient_tracker');
---
+++ models/records.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recordsSchema = new Schema({
+    doctor_name:{
+        type:String,
+        required:true,
+    },
+    patient_caseNo:{
+        type:String,
+    },
+    date:{
+        type:Date,
+        required:true,
+    },
+    chief_complaint:{
+        type:String,
+        required:true,
+    },
+    physical_examination:{
+        type:String,
+    },
+    history_of_illness:{
+        type:String,
+    },
+    diagnosis:{
+        type:String,
+        required:true,
+    },
+    blood_pressure:{
+        type:Number,
+    },
+    respiratory_rate:{
+        type:Number,
+    },
+    capillary_refill:{
+        type:Number,
+    },
+    temperature:{
+        type:Number,
+    },
+    weight:{
+        type:Number,
+    },
+    pulse_rate:{
+        type:Number,
+    },
+    medication_treatment:{
+        type:String,
+        required:true,
+    },
+
+
+})
+
+const Records= new mongoose.model("Records",recordsSchema)
+module.exports=Records;
