2c54ab2b78557e799250036d1c5a8f61b00755e8
VallabhSangvikar <vallabhdsangvikar18@gmail.com>
2024-04-25 22:38:53 +0530

    basic setup

---
+++ app.js
@@ -0,0 +1,29 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const port=4000;
+
+const methodOverride=require("method-override");
+app.use(methodOverride("_method"));
+
+
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set(path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/patient_tracker');
+}
+
+
+app.listen(port,()=>{
+    console.log(`server is running `);
+    
+});
